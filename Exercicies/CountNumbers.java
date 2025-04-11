d261ee70c243f41a5ec61074d924625284c4d5c7
JoaoVignoli <vignolijoao@gmail.com>
2025-04-11 20:45:14 -0300

    Contador de 1 até 10.

---
+++ Exercicies/CountNumbers.java
@@ -0,0 +1,12 @@
+package ModuleOne;
+
+import java.util.Scanner;
+
+public class CountNumbers {
+    public static void main(String[] args){
+        int number = 10;
+        for (int i = 1; i <= number;i++){
+            System.out.println(i);
+        }
+    }
+}
